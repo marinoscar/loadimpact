7c6e3e166270f394cf33cc047c5971f58d423d95
Oscar Marin <oscar.marin@entegreat.com>
2013-10-06 21:38:32 -0600

    fix security issue

--- Program.cs
+++ Program.cs
@@ -10,7 +10,15 @@ namespace LoadImpact
     {
         static void Main(string[] args)
         {
-            var api = new RestApi("0dfb7b336a3d1ed03d16f9e00ab0791356814de2317f46498d12434a3a4c82f9");
+            var consoleArgs = new Arguments(args);
+            var apiKey = "";
+            if (!consoleArgs.IsSwitchPresent("/apiKey"))
+            {
+                Console.WriteLine("Enter your api key");
+                apiKey = Console.ReadLine();
+            }
+            Console.WriteLine("Enter your api key");
+            var api = new RestApi(apiKey);
             var start = DateTime.Now;
             Console.WriteLine("Starting to get data at {0}", start);
             api.StoreResultsInFile(@"C:\EWM\TestResults.csv");
